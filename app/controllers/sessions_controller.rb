4ad26f573fb84f1a039c55e082d2098ab3443fe0
OE16 - R - Phan Van Vuong <vuongphan139.it@gmail.com>
2018-11-23 13:29:30 +0700

    Chapter 9: Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,29 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
 
-    if user&.user.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    if user && user.authenticate(params[:session][:password])
+      login_success user
     else
-      flash[:danger] = t "invalid_email_password"
-      render :new
+      login_fail
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def login_fail
+    flash[:danger] = t "invalid_email_password"
+    render :new
+  end
+
+  def login_success user
+    log_in user
+    remember_box_value = params[:session][:remember_me]
+    remember_box_value == Settings.remember_me ? remember(user) : forget(user)
+    redirect_to user
   end
 end
