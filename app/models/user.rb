d22f3366d843965a30a4aae31b651b315ab22020
OE16 - R - Phan Van Vuong <vuongphan139.it@gmail.com>
2018-11-23 13:29:30 +0700

    Chapter 8: Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save :email_downcase
+  before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true,
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   has_secure_password
 
   private
-  
-  def email_downcase
+
+  def downcase_email
     email.downcase!
   end
 end
